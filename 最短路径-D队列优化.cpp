41fb7ffe916e15367f52639998f077cf9bc21576
happy163 <1985579197@qq.com>
2018-07-02 12:23:30 +0800

    添加路径

--- 最短路径-D队列优化.cpp
+++ 最短路径-D队列优化.cpp
@@ -1,38 +1,39 @@
 #define INF 10000000
 
 struct node {
-	int x, d;
-	node() {};
-	node(int a, int b) {
-		x = a;
-		d = b;
-	}
-	bool operator < (const node &a) const
-	{
-		return (a.d == d ? x < a.x : d < a.d);
-	}
+    int x, d;
+    node() {};
+    node(int a, int b) {
+        x = a;
+        d = b;
+    }
+    bool operator<(const node &a) const {
+        return (a.d == d ? x < a.x : d < a.d);
+    }
 };
 
 vector<node> eg[101];
 int dist[101];
-void dfs(int s, int n)
-{
-	for (int i = 1; i <= n; i++) { 
-		dist[i] = INF; 
-	}
-	dist[s] = 0;
-	priority_queue<node> list;
-	list.push(node(s, dist[s]));
-	while (!list.empty()) {
-		node x = list.top();
-		list.pop();
-		for (vector<node>::iterator i = eg[x.x].begin(); i != eg[x.x].end(); i++) {
-			node y = *i;
-			if (dist[y.x]>x.d + y.d)
-			{
-				dist[y.x] = x.d + y.d;
-				list.push(node(y.x, dist[y.x]));
-			}
-		}
-	}
+vector<int> path[101];
+
+void dfs(int s, int n) {
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INF;
+    }
+    dist[s] = 0;
+    priority_queue<node> list;
+    list.push(node(s, dist[s]));
+    while (!list.empty()) {
+        node x = list.top();
+        list.pop();
+        for (vector<node>::iterator i = eg[x.x].begin(); i != eg[x.x].end(); i++) {
+            node y = *i;
+            if (dist[y.x] > x.d + y.d) {
+                dist[y.x] = x.d + y.d;
+                path[y.x] = path[x.x];
+                path[y.x].push_back(y.x);
+                list.push(node(y.x, dist[y.x]));
+            }
+        }
+    }
 }
