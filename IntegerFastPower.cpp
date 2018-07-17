390394e5b7048c6a90def700a3065ccb2e27c4c0
happy233 <happier233@qq.com>
2018-07-17 13:54:08 +0800

    更新代码

--- IntegerFastPower.cpp
+++ IntegerFastPower.cpp
@@ -1,14 +1,8 @@
-int fast_pow(int a, int b) {
-	//a %= 1000;
-	int r = 1, base = a;
-	while (b) {
-		if (b & 1) {
-			r *= base;
-			//r %= 1000;
-		}
-		base *= base;
-		//base %= 1000;
-		b >>= 1;
-	}
-	return r;
+int fast_pow(int a, int b, int p) {
+    int r = 1, base = a % p;
+    for(;b;b>>1){
+        if (b & 1)  r = (r * base) % p;
+        base = (base * base) % p;
+    }
+    return r;
 }
