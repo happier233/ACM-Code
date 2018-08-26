42b3e98ac3bf66d4a40e61e4c3c12d3e8fef52c1
happy163 <1985579197@qq.com>
2018-08-26 09:30:07 +0800

    更新文件 MinSpanTree.cpp 冲突

--- MinSpanTree.cpp
+++ MinSpanTree.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define INF 1000000
 #define N 100000
 ll lowcost[N];//此数组用来记录第j个节点到其余节点最少花费
@@ -36,34 +35,3 @@ ll prim() {
     }
     return mincost;
 }
-=======
-#define INF 0x3f3f3f
-int lowcost[110];//此数组用来记录第j个节点到其余节点最少花费
-int map[110][110];//用来记录第i个节点到其余n-1个节点的距离
-int visit[110];//用来记录最小生成树中的节点
-int city;
-
-int prim() {
-    int min, i, j, next, mincost = 0;
-    memset(visit, 0, sizeof(visit));
-    for (i = 1; i <= city; i++) lowcost[i] = map[1][i];
-    visit[1] = 1;
-    for (i = 1; i < city; i++) {
-        min = INF;
-        for (j = 1; j <= city; j++) {
-            if (!visit[j] && min > lowcost[j]) {
-                min = lowcost[j];
-                next = j;
-            }
-        }
-        if (min == INF) return INF;
-        mincost += min;
-        visit[next] = 1;
-        for (j = 1; j <= city; j++) {
-            if (!visit[j] && lowcost[j] > map[next][j])
-                lowcost[j] = map[next][j];
-        }
-    }
-    return mincost;
-}
->>>>>>> refs/remotes/origin/master
