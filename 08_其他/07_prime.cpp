df567e3c127cd946129c122e23a521de7faa356f
happy233 <1985579197@qq.com>
2019-09-07 11:54:25 +0800

    修复边界问题

--- 08_其他/07_prime.cpp
+++ 08_其他/07_prime.cpp
@@ -10,7 +10,7 @@ void initPrime() {
         if (!prime_list[i])
             prime_table[prime_count++] = i;
         for (int j = 0, e = PMAX / i;
-             j < prime_count && prime_table[j] <= e; j++) {
+             j < prime_count && prime_table[j] < e; j++) {
             prime_list[i * prime_table[j]] = true;
             if (i % prime_table[j] == 0) break;
         }
@@ -30,7 +30,7 @@ void initPrime() {
         if (!prime_list[i])
             prime_list[i] = prime_table[prime_count++] = i;
         for (int j = 0, e = PMAX / i, now; 
-        	j < prime_count && (now = prime_table[j]) <= e; j++) {
+        	j < prime_count && (now = prime_table[j]) < e; j++) {
             prime_list[i * now] = now;
             if (i % now == 0) break;
         }
