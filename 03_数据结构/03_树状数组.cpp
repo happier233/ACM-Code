25f22faad9259368e18eeea6e1ff20b07c6f8bcc
happier233 <happier233@qq.com>
2019-09-09 14:09:39 +0800

    更新树状数组
    增加区间查询区间更新模板

--- 03_数据结构/03_树状数组.cpp
+++ 03_数据结构/03_树状数组.cpp
@@ -4,8 +4,8 @@ struct BITree {
     ll c[N];
 
     void init(int _n) {
-        n = _n;
-        memset(c, 0, sizeof(ll) * ++n);
+        n = _n + 1;
+        memset(c, 0, sizeof(ll) * n);
     }
 
     void change(int pos, ll v) {
@@ -24,4 +24,37 @@ struct BITree {
         change(l, v);
         change(r + 1, -v);
     }
-};
+} tree;
+
+// 区间更新区间查询
+struct BITree {
+    int n;
+    ll c[N], d[N];
+
+    void init(int _n) {
+        n = _n + 1;
+        memset(c, 0, sizeof(ll) * n);
+        memset(d, 0, sizeof(ll) * n);
+    }
+
+    void change(int pos, ll v) {
+        for (int i = pos; i < n; i += i & (-i))
+            c[i] += v, d[i] += v * (pos - 1);
+    }
+
+    ll query(int x) {
+        ll ans = 0;
+        for (int i = x; i > 0; i -= i & (-i))
+            ans += x * c[i] - d[i];
+        return ans;
+    }
+
+    void update(int l, int r, ll v) {
+        change(l, v);
+        change(r + 1, -v);
+    }
+
+    ll sum(int l, int r) {
+        return query(r) - query(l - 1);
+    }
+} tree;
