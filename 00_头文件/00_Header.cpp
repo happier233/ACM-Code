ff5cb450b4dd2e127b0df96db4c21dbdf5947bbc
happy233 <1985579197@qq.com>
2019-09-19 10:10:37 +0800

    修改头文件，删除stdc++，更新模板pdf

--- 00_头文件/00_Header.cpp
+++ 00_头文件/00_Header.cpp
@@ -5,18 +5,46 @@
 using namespace std;
 
 //-----
+typedef double db;
 typedef long long ll;
+typedef unsigned int ui;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define fi first
+#define se second
 #define pw(x) (1ll << (x))
+#define bt(x, k) (((x) >> k) & 1)
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(),(x).end()
 #define rep(i, l, r) for(int i=(l);i<(r);++i)
 #define per(i, l, r) for(int i=(r)-1;i>=(l);--i)
 #define mst(t, v, n) memset(t, v, sizeof(decltype(*(t))) * (n))
 #define sf(x) scanf("%d", &(x))
-//---debug array *(ll(*)[10])ptr
+#ifndef ACM_LOCAL
+#define endl '\n'
+#endif
 
-using namespace std;
+int main() {
+#ifdef ACM_LOCAL
+    freopen("./data/std.in", "r", stdin);
+    // freopen("./data/std.out", "w", stdout);
+#else
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#endif
 
-const double pi = acos(-1);
+#ifdef ACM_LOCAL
+    auto start = clock();
+#endif
+    int t = 1;
+//    cin >> t;
+    while (t--)
+        solve();
+#ifdef ACM_LOCAL
+    auto end = clock();
+    cerr << "Run Time: " << double(end - start) / CLOCKS_PER_SEC << "s" << endl;
+#endif
+    return 0;
+}
