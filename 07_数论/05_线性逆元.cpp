d872e76fc2ff00b0a9f3b505e8b994599d98fd10
happier233 <happier233@qq.com>
2019-12-10 11:18:24 +0800

    添加线性逆元模板

---
+++ 07_数论/05_线性逆元.cpp
@@ -0,0 +1,10 @@
+// 逆元，阶乘，阶乘逆元
+ll inv[N] = {1, 1}, fac[N] = {1, 1}, ifac[N] = {1, 1};
+// 线性求逆元
+void init() {
+    for (int i = 2; i < N; i++) {
+        inv[i] = (MOD - (MOD / i)) * inv[MOD % i] % MOD;
+        fac[i] = fac[i - 1] * i % MOD;
+        ifac[i] = ifac[i - 1] * inv[i] % MOD;
+    }
+}
