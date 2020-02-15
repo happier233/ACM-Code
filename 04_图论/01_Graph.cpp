d1f96aba8f0fde12cbab888eea6206d4872dbd73
happier233 <happier233@qq.com>
2020-02-15 12:24:39 +0800

    修改建图结构

--- 04_图论/01_Graph.cpp
+++ 04_图论/01_Graph.cpp
@@ -1,32 +1,34 @@
-#define forg(i, h, eg) for(int i = (h); ~i; i = (eg[i]).nxt)
+#define forg(i, h, eg) for(int i = (h); ~i; i = (eg[i]).first)
 
 struct Edge {
-    int e, nxt;
-    ll v;
-    Edge() = default;
-    Edge(int a, ll b, int c = 0) : e(a), v(b), nxt(c) {}
-
-    bool operator<(const Edge &a) const {
-        return (a.v == v ? e < a.e : v < a.v);
-    }
+    int to;
+    ll w;
 };
 
+inline bool operator<(Edge a, Edge b) {
+    return a.w < b.w;
+}
+
 const ll INF = ll(1e11);
-const int N = int(1e5 + 10);
-const int M = int(3e5 + 10);
+const int N = int(1e6 + 10);
+const int M = int(2e6 + 10);
 
 struct Graph {
-    Edge eg[M];
+    pair<int, Edge> eg[M];
     int head[N];
-    int cnt;
+    int tot;
 
     void init(int n) {
         memset(head, -1, sizeof(int) * ++n);
-        cnt = 0;
+        tot = 0;
     }
 
-    inline void addEdge(int x, int y, ll v = 0) {
-        eg[cnt] = Edge(y, v, head[x]);
-        head[x] = cnt++;
+    inline void addEdge(int x, Edge g) {
+        eg[tot] = {head[x], g};
+        head[x] = tot++;
+    }
+    
+    Edge &operator[](int p) {
+        return eg[p].second;
     }
 } gh;
