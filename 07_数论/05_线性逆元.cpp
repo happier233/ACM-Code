5b24f44edd354a8c2eef39cea619a9471a8d4176
happier233 <happier233@qq.com>
2020-02-15 12:37:50 +0800

    增加注释

--- 07_数论/05_线性逆元.cpp
+++ 07_数论/05_线性逆元.cpp
@@ -2,6 +2,7 @@
 ll inv[N] = {1, 1}, fac[N] = {1, 1}, ifac[N] = {1, 1};
 // 线性求逆元
 void init() {
+	// 如果用的Int记得下面inv[i] = 1ll*...
     for (int i = 2; i < N; i++) {
         inv[i] = (MOD - (MOD / i)) * inv[MOD % i] % MOD;
         fac[i] = fac[i - 1] * i % MOD;
