3ad4ed7a83a5d5bbd4a9993a2b5efc529f59073c
happier233 <happier233@qq.com>
2020-02-15 12:35:47 +0800

    重写二维线段树模板，只有查询没有修改

--- 03_数据结构/06_二维线段树.cpp
+++ 03_数据结构/06_二维线段树.cpp
@@ -1,141 +1,99 @@
-
-const int N = 1005;
-
+const int N = int(2e3 + 48);
+ 
+ll p[N][N];
+ 
 struct SegTree {
-
-    inline int son(int k, int x) {
-        return (k << 2) - 2 + x;
+ 
+    inline int static get(int l, int r) {
+        return (l + r) | (l != r);
     }
-
+ 
     struct node {
         int l, r;
-
-        node() = default;
-
-        node(int a, int b) : l(a), r(b) {}
-
-        inline int mid() {
-            return (l + r) >> 1;
-        }
-
-        inline node left() {
-            return node(l, mid());
-        }
-
-        inline node right() {
-            return node(mid() + 1, r);
-        }
-
-        inline bool in(int x) {
-            return x >= l && x <= r;
-        }
-
-        inline bool more() {
-            return l < r;
-        }
-
-        bool operator==(const node &t) {
-            return l == t.l && r == t.r;
-        }
+ 
+        inline int mid() { return (l + r) >> 1; }
+ 
+        inline bool point() { return l == r; }
+ 
+        inline node operator--(int) { return {l, mid()}; }
+ 
+        inline node operator++(int) { return {mid() + 1, r}; }
+ 
+        bool operator==(node t) const { return l == t.l && r == t.r; }
+ 
+        operator int() { return get(l, r); }
     };
-
-    ll c[N << 2][N << 2];
-    ll ans[N << 4];
-    ll laz[N << 4];
-
-    inline void up(int k, bool x, bool y) {
-        int s = (k << 2) - 2;
-        ll t = 0;
-        if (x) t += ans[s] + ans[s + 1] + laz[s] + laz[s + 1];
-        if (y) t += ans[s + 2] + ans[s + 3] + laz[s + 2] + laz[s + 3];
-        ans[k] = t;
+ 
+    ll s[N * 2][N * 2];
+ 
+    inline void upX(node x, node y) {
+        s[x][y] = max(s[x--][y], s[x++][y]);
     }
-
-    inline void push(int k) {
-        int s = (k << 2) - 2;
-        laz[s] += laz[k];
-        laz[s + 1] += laz[k];
-        laz[s + 2] += laz[k];
-        laz[s + 3] += laz[k];
-        ans[k] += laz[k];
-        laz[k] = 0;
+ 
+    inline void upY(node x, node y) {
+        s[x][y] = max(s[x][y--], s[x][y++]);
     }
-
-    void build(node x, node y, int k) {
-        laz[k] = 0;
-        if (x.more() && y.more()) {
-            ans[k] = c[x.l][y.l];
+ 
+    void buildY(node x, node y) {
+        if (y.point()) {
+            s[x][y] = p[x.l][y.l];
             return;
         }
-        ans[k] = 0;
-        bool ax = false;
-        bool ay = false;
-        if (x.more()) {
-            build(x.left(), y, son(k, 0));
-            build(x.right(), y, son(k, 1));
-        }
-        if (y.more()) {
-            build(x, y.left(), son(k, 2));
-            build(x, y.right(), son(k, 3));
-        }
-        up(k, x.more(), y.more());
+        buildY(x, y--);
+        buildY(x, y++);
+        upY(x, y);
+        return;
     }
-
-    void change(node x, node y, int k, node l, node r, ll v) {
-        if (x == l && y == r) {
-            laz[k] += v;
+ 
+    void mergeY(node x, node y) {
+        if (y.point()) {
+            upX(x, y);
             return;
         }
-        push(k);
-        if (x.more()) {
-            if (l.r <= x.mid()) {
-                change(x.left(), y, son(k, 0), l, r, v);
-            } else if (l.l > x.mid()) {
-                change(x.right(), y, son(k, 1), l, r, v);
-            } else {
-                change(x.left(), y, son(k, 0), node(l.l, x.mid()), r, v);
-                change(x.right(), y, son(k, 1), node(x.mid() + 1, l.r), r, v);
-            }
-        }
-        if (y.more()) {
-            if (r.l <= y.mid()) {
-                change(x, y.left(), son(k, 2), l, r, v);
-            } else if (r.r > y.mid()) {
-                change(x, y.right(), son(k, 3), l, r, v);
-            } else {
-                change(x, y.left(), son(k, 2), l, node(r.l, y.mid()), v);
-                change(x, y.right(), son(k, 3), l, node(y.mid() + 1, r.r), v);
-            }
-        }
-        up(k, x.more(), y.more());
+        mergeY(x, y--);
+        mergeY(x, y++);
+        upX(x, y);
+        return;
     }
-
-    ll query(node x, node y, int k, node l, node r) {
-        if (x == l && y == r) {
-            return ans[k] + laz[k];
+ 
+    void build(node x, node y) {
+        if (x.point()) {
+            buildY(x, y);
+            return;
         }
-        push(k);
-        ll t = 0;
-        if (x.more()) {
-            if (l.r <= x.mid()) {
-                t += query(x.left(), y, son(k, 0), l, r);
-            } else if (l.l > x.mid()) {
-                t += query(x.right(), y, son(k, 1), l, r);
-            } else {
-                t += query(x.left(), y, son(k, 0), node(l.l, x.mid()), r);
-                t += query(x.right(), y, son(k, 1), node(x.mid() + 1, l.r), r);
-            }
-        }
-        if (y.more()) {
-            if (r.l <= y.mid()) {
-                t += query(x, y.left(), son(k, 2), l, r);
-            } else if (r.r > y.mid()) {
-                t += query(x, y.right(), son(k, 3), l, r);
-            } else {
-                t += query(x, y.left(), son(k, 2), l, node(r.l, y.mid()));
-                t += query(x, y.right(), son(k, 3), l, node(y.mid() + 1, r.r));
-            }
-        }
-        return t;
+        build(x--, y);
+        build(x++, y);
+        mergeY(x, y);
+        return;
     }
-};
+ 
+    ll queryY(node x, node y, node a, node b) {
+        if (y == b) {
+            return s[x][y];
+        }
+        int mid = y.mid();
+        if (b.r <= mid) {
+            return queryY(x, y--, a, b);
+        } else if (b.l > mid) {
+            return queryY(x, y++, a, b);
+        } else {
+            return max(queryY(x, y--, a, {b.l, mid}),
+                       queryY(x, y++, a, {mid + 1, b.r}));
+        }
+    }
+ 
+    ll query(node x, node y, node a, node b) {
+        if (x == a) {
+            return queryY(x, y, a, b);
+        }
+        int mid = x.mid();
+        if (a.r <= mid) {
+            return query(x--, y, a, b);
+        } else if (a.l > mid) {
+            return query(x++, y, a, b);
+        } else {
+            return max(query(x--, y, {a.l, mid}, b),
+                       query(x++, y, {mid + 1, a.r}, b));
+        }
+    }
+} tree;
